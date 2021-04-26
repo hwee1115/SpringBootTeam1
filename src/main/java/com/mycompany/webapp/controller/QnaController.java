52a25cfa5dd9237d1bf596344b2f4aba7726bf26
jeongyewon <yeewonee@naver.com>
2021-04-26 11:17:19 +0900

    예원 수정

--- src/main/java/com/mycompany/webapp/controller/QnaController.java
+++ src/main/java/com/mycompany/webapp/controller/QnaController.java
@@ -36,6 +36,7 @@ public class QnaController {
       Map<String, Object> map = new HashMap<>(); //map객체 만듦
       map.put("pager", pager);
       map.put("qnas", list);
+      map.put("count", totalRows);
       return map; 
    }
    
